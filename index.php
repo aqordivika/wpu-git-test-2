23cf90acdfda7e338594b0959b5bcbc226cca853
Aqordeon <aqordivika@yahoo.co.id>
2020-04-16 14:12:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque magnam porro dolores, possimus aspernatur amet eius iure facere in optio mollitia eaque asperiores provident consequuntur fugit impedit maiores tempora unde!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
